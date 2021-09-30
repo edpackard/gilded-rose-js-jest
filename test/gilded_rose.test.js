c752a4b87df078a501a5ce1492a217306e0e935d
edpackard <edpackard81@gmail.com>
2021-09-30 09:35:13 +0100

    add and refactor basic functionality tests

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,15 +1,46 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
-describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+let items;
+let lembas = new Item("lembas bread", 30, 25);
+let entDraught = new Item("ent-draught", 20, 40);
+let oneRing = new Item("one ring", 40, 50);
+
+describe("Gilded Rose", () => {
+  describe("basic functions: single item", () => {
+    beforeAll(() => {
+      const gildedRose = new Shop([lembas]);
+      items = gildedRose.updateQuality();
+    });
+
+    it("should store name of item", () => {
+      expect(items[0].name).toBe("lembas bread");
+    });
+
+    it("should reduce sellIn value at end of day", () => {
+      expect(items[0].sellIn).toBe(29);
+    });
+
+    it("should degrade item quality at end of day", () => {
+      expect(items[0].quality).toBe(24);
+    });
   });
 
-  it("should degrade item quality at end of day", function () {
-    const gildedRose = new Shop([new Item("lembas bread", 20, 40)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(39);
+  describe("basic functions: multiple items", () => {
+    beforeAll(() => {
+      const gildedRose = new Shop([lembas, entDraught, oneRing]);
+      items = gildedRose.updateQuality();
+    });
+
+    it("should store names of items", () => {
+      expect(items[1].name).toBe("ent-draught");
+    });
+
+    it("should reduce sellIn value at end of day", () => {
+      expect(items[2].sellIn).toBe(39);
+    });
+
+    it("should degrade item quality at end of day", () => {
+      expect(items[1].quality).toBe(39);
+    });
   });
 });
