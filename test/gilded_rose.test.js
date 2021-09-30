2d616800a87eddec00dabe8457106a9f9fb6399a
edpackard <edpackard81@gmail.com>
2021-09-30 09:01:52 +0100

    add test to check item quality degraded at end of day

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -6,4 +6,10 @@ describe("Gilded Rose", function () {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
   });
+
+  it("should degrade item quality at end of day", function () {
+    const gildedRose = new Shop([new Item("lembas bread", 20, 40)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(39);
+  });
 });
