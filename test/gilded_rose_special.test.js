76ca19d4d178ea8d71b4c208073776af38617bf1
edpackard <edpackard81@gmail.com>
2021-09-30 16:38:23 +0100

    refactor and update aged brie tests

--- test/gilded_rose_special.test.js
+++ test/gilded_rose_special.test.js
@@ -5,16 +5,26 @@ let gildedRose;
 
 describe("Gilded Rose: special item tests", () => {
   describe("Aged Brie", () => {
-    it("rises +1 in quality per day before sellIn passed", () => {
-      brie = { name: "Aged Brie", sellIn: 30, quality: 25 };
+    beforeEach(() => {
+      brie = { name: "Aged Brie", sellIn: 10, quality: 25 };
       gildedRose = new Shop([brie]);
+    });
+
+    it("rises +1 in quality per day before sellIn passed", () => {
       items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(26);
     });
+
+    it("cannot have a quality above 50", () => {
+      for (var i = 0; i < 50; i++) {
+        items = gildedRose.updateQuality();
+      }
+      expect(items[0].quality).toBe(50);
+    });
+
     it("rises +2 in quality per day after sellIn passed", () => {
       brie = { name: "Aged Brie", sellIn: 0, quality: 25 };
       gildedRose = new Shop([brie]);
-      console.log(gildedRose.items[0]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(27);
     });
