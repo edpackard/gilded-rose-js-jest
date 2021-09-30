28ba18996999060fda25885b614821c024287390
edpackard <edpackard81@gmail.com>
2021-09-30 14:26:04 +0100

    add tests for basic quality behaviours

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,14 +1,14 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 let items;
-let lembas = new Item("lembas bread", 30, 25);
-let entDraught = new Item("ent-draught", 20, 40);
-let oneRing = new Item("one ring", 40, 50);
+let gildedRose;
+let lembas;
 
 describe("Gilded Rose", () => {
   describe("basic functions: single item", () => {
     beforeAll(() => {
-      const gildedRose = new Shop([lembas]);
+      lembas = new Item("lembas bread", 30, 25);
+      gildedRose = new Shop([lembas]);
       items = gildedRose.updateQuality();
     });
 
@@ -27,7 +27,10 @@ describe("Gilded Rose", () => {
 
   describe("basic functions: multiple items", () => {
     beforeAll(() => {
-      const gildedRose = new Shop([lembas, entDraught, oneRing]);
+      lembas = new Item("lembas bread", 30, 25);
+      let entDraught = new Item("ent-draught", 20, 40);
+      let oneRing = new Item("one ring", 40, 50);
+      gildedRose = new Shop([lembas, entDraught, oneRing]);
       items = gildedRose.updateQuality();
     });
 
@@ -44,6 +47,26 @@ describe("Gilded Rose", () => {
     it("should degrade item quality at end of day", () => {
       expect(items[1].quality).toBe(39);
       expect(items[2].quality).toBe(49);
+    });
+  });
+
+  describe("basic quality behaviours", () => {
+    it("does not degrade quality below zero", () => {
+      lembas = new Item("lembas bread", 30, 25);
+      gildedRose = new Shop([lembas]);
+      for (var i = 0; i < 26; i++) {
+        items = gildedRose.updateQuality();
+      }
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("doubles rate of quality degradation once sellIn less than zero", () => {
+      let pipeweed = new Item("pipeweed", 5, 30);
+      gildedRose = new Shop([pipeweed]);
+      for (var i = 0; i < 10; i++) {
+        items = gildedRose.updateQuality();
+      }
+      expect(items[0].quality).toBe(15);
     });
   });
 });
