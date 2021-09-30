592d0e51024cca384be39c392e90c64341884eef
edpackard <edpackard81@gmail.com>
2021-09-30 16:11:08 +0100

    finish aged brie tests

--- test/gilded_rose_special.test.js
+++ test/gilded_rose_special.test.js
@@ -5,11 +5,18 @@ let gildedRose;
 
 describe("Gilded Rose: special item tests", () => {
   describe("Aged Brie", () => {
-    it("rises in quality per day", () => {
+    it("rises +1 in quality per day before sellIn passed", () => {
       brie = { name: "Aged Brie", sellIn: 30, quality: 25 };
       gildedRose = new Shop([brie]);
       items = gildedRose.updateQuality();
       expect(items[0].quality).toBe(26);
+    });
+    it("rises +2 in quality per day after sellIn passed", () => {
+      brie = { name: "Aged Brie", sellIn: 0, quality: 25 };
+      gildedRose = new Shop([brie]);
+      console.log(gildedRose.items[0]);
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(27);
     });
   });
 });
